36dcbb15dacc47151c7ad553ca2e9460598e6aab
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-24 01:12:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ updatePlaylist(playlist, "My Bloody Valentine", "Sometimes");
 updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+  delete playlist.artistName;
 }
 
 removeFromPlaylist(playlist, "Slowdrive");
