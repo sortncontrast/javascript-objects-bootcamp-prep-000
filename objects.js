0cfe4a156828de4d39d4bca6b9bed282436f8b73
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-24 01:12:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,15 @@
-var playlist = {Beyonce: "Crazy In Love"};
+var playlist = {Slowdrive: "Alison"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
 
-updatePlaylist(playlist, "Drake", "Hotline Bling");
+updatePlaylist(playlist, "My Bloody Valentine", "Sometimes");
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
 }
 
-removeFromPlaylist(playlist, "Beyonce");
+removeFromPlaylist(playlist, "Slowdrive");
